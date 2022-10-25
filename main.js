9c268cb0390d1938fe338a6d7d8a5e684a9b941c
ayaanshsharma <98742867+ayaanshsharma@users.noreply.github.com>
2022-10-25 14:35:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+img="";
+status="";
+
+function preload(){
+    img = loadImage("moris_garage.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+if(error){
+    console.log(error);
+}
+console.log(results);
+document.getElementById("status").innerHTML = "Status: Object";
+}
